2c78988e0488e3459d7e54520d92846ce7556a1e
fuxiangmaster <253658626@qq.com>
2016-08-29 11:41:27 +0800

    Add files via upload

---
+++ helloworld/helloworld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloworld
+//
+//  Created by 伏祥 on 16/8/29.
+//  Copyright © 2016年 fuxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helloworld/helloworld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  helloworld
+//
+//  Created by 伏祥 on 16/8/29.
+//  Copyright © 2016年 fuxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
